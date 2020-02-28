3b8eb2d457bcc264d4a3e480936dfdf0c368eafc
Bryce Tucker <bryce.tucker@gmail.com>
2020-02-28 16:25:23 +0000

    App is connecting to heroku back end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FactsContainer from './components/FactsContainer';
+
+import { withAuthenticator } from 'aws-amplify-react';
+import Amplify, { Auth } from 'aws-amplify';
+
+Amplify.configure({
+    Auth: {
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-west-2',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-west-2_q9IkW7VmH',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '6qoqfmp5jnlsevn17r4e1ha6ao',
+    }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jwtToken: null
+    }
+    
+  }
+  
+  componentDidMount() {
+    Auth.currentAuthenticatedUser({
+        bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    }).then(user => { 
+      this.setState({jwtToken: user.signInUserSession.idToken.jwtToken});
+    })
+    .catch(err => console.log(err));
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          
+          <FactsContainer idToken={this.state.jwtToken} />
+        </header>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App);
